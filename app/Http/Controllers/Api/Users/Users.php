7b08c86f6c16e893f93bc7f0f6fe6de076e32e28
merna <mernamousa209@gmail.com>
2021-10-28 18:22:09 +0200

    category service

--- app/Http/Controllers/Api/Users/Users.php
+++ app/Http/Controllers/Api/Users/Users.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api\Users;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Vendor;
+use App\Models\Category;
+use App\Models\Item;
+use App\Models\Sub_category;
 use URL;
 
 class Users extends Controller
@@ -13,7 +16,7 @@ class Users extends Controller
 
     public function vendors(){
         $data['status'] = true;
-        $data['vendors'] = Vendor::get(['vendor_name','vendor_image']);
+        $data['vendors'] = Vendor::get(['id','vendor_name','vendor_image']);
 
         if(!empty($data['vendors'])){
             foreach($data['vendors'] as $vend){
@@ -21,6 +24,38 @@ class Users extends Controller
             }
         }
         return $data;
+    }
+
+
+
+
+    public function categories($vendor_id=false){
+        $data['status'] = true;
+        $data['categories'] = [];
+
+        if($vendor_id == false){
+            $data['categories'] = Category::get();
+        }else{
+            //"cat->sub_cat->item -> vendor_id"
+
+            $vendor_sub_cats_id = Item::where('vendor_id',$vendor_id)->pluck('sub_cat_id');
+            if(!empty($vendor_sub_cats_id)) {
+                $vendor_cats_id = Sub_category::whereIn('id',$vendor_sub_cats_id)->pluck('cat_id');
+                if(!empty($vendor_cats_id)) {
+                    $data['categories'] = Category::whereIn('id',$vendor_cats_id)->get();
+                }
+            }
+        }
+
+
+        if (!empty($data['categories'])) {
+            foreach($data['categories'] as $cat){
+                $cat->categoryImage = URL::to('Admin_uploads/categories/'.$cat->categoryImage);
+            }
+        }
+
+        return $data;
+
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -11,5 +11,6 @@ Route::group(['namespace'=>'Api\Users'],function(){
 
     Route::post('userCountery','Users_auth@userCountery');
     Route::get('vendors','Users@vendors');
+    Route::get('categories/{vendor_id?}','Users@categories');
 
 });
