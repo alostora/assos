c4de55a0fbaf4cc1150dc2243c84a6a1d285fe7b
merna <mernamousa209@gmail.com>
2021-11-06 18:07:43 +0200

    sub properties

--- app/Http/Controllers/Admin/Properties.php
+++ app/Http/Controllers/Admin/Properties.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Property;
+use App\Models\Sub_property;
 
 class Properties extends Controller
 {
@@ -43,11 +44,41 @@ class Properties extends Controller
 
 
 
-
+    ////sub property
     public function sub_propertiesInfo($propId){
-
-       return $propId;
+       $data['properties'] = Sub_property::where('prop_id',$propId)->get();
+       return view('Admin/Properties/sub_propertiesInfo',$data);
     }
+
+
+
+
+    public function sub_viewCreateProperty($id=false){
+        $data['property'] = false;
+        if ($id != false){
+            $data['property'] = Sub_property::find($id);
+        }
+        return view('Admin/Properties/sub_viewCreateProperty',$data);
+    }
+
+
+
+
+
+
+    public function sub_createProperty($id){
+        if ($data['id'] == null) {
+            Sub_property::create($data);
+        }else{
+            Sub_property::where('id',$data['id'])->update($data);
+        }
+        session()->flash('success','Done');
+        return back();
+    }
+
+
+
+
 
 
 
---
+++ resources/views/Admin/layouts/Properties/sub_viewCreateProperty.blade.php
@@ -0,0 +1,50 @@
+<div class="content-wrapper">
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box box-info">
+          <div class="box-header with-border">
+            <h3 class="box-title">@if(empty($property)) @lang('leftsidebar.Add') @else @lang('leftsidebar.Edit') @endif</h3>
+          </div>
+          @if(count($errors))
+            @foreach($errors->all() as $error)
+              <div class="col-sm-12">
+                <div class="alert alert-danger">{{$error}}</div>
+              </div>
+            @endforeach
+          @endif
+          @if(session()->has('success'))
+            <div class="alert alert-success">{{session('success')}}</div>
+          @endif
+          @if(session()->has('warning'))
+            <div class="alert alert-warning">{{session('warning')}}</div>
+          @endif
+          <form class="form-horizontal" action="{{url('admin/sub_createProperty')}}" method="post" enctype="multipart/form-data">
+            <div class="box-body">
+              @csrf
+              <input type="hidden" name="id" value="@if(!empty($property)) {{$property->id}} @endif">
+
+              <div class="form-group">
+
+                  <label for="propertyName" class="col-sm-2 control-label">
+                  @lang('leftsidebar.propertyName')</label>
+                  <div class="col-sm-4">
+                      <input type="text" name="propertyName" class="form-control" id="propertyName" placeholder="@lang('leftsidebar.propertyName')" value="@if(!empty($property)) {{$property->propertyName}} @endif" required>
+                  </div>
+                
+                  <label for="propertyNameAr" class="col-sm-2 control-label">@lang('leftsidebar.propertyNameAr')</label>
+                  <div class="col-sm-4">
+                      <input type="text" name="propertyNameAr" class="form-control" placeholder="@lang('leftsidebar.propertyNameAr')" value="@if(!empty($property)) {{$property->propertyNameAr}} @endif" required id="propertyNameAr">
+                  </div>
+              </div>
+
+            </div>
+            <div class="box-footer">
+                <input type="submit" class="btn btn-info" value="@if(empty($property)) @lang('leftsidebar.Add') @else @lang('leftsidebar.Edit') @endif">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
